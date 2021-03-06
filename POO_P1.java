58db4852bb24be0fbe29b77218d98c48f033da6c
Edgar Rivera <80140422+edgar06un@users.noreply.github.com>
2021-03-05 22:10:31 -0600

    Add files via upload

---
+++ POO_P1.java
@@ -0,0 +1,41 @@
+/*
+ *Estos son comentarios en el programa, donde los simbolos 
+  diagonal-asterisco crea un comentario de varias lineas.  
+ */
+
+//Paquete donde se almacenan todas nuestras clases.
+package poo_p1;
+/**
+ *
+ *@author Edgar Rivera Alvarez 
+ */
+
+//Nombre de nuestra clase, necesita tener el mismo nombre que el archivo.  
+public class POO_P1 {
+
+    /**
+     * Comentarios de documentación
+     * @param args the command line arguments
+     */
+    
+    /*
+    *Después tenemos la definición de nuestro método main
+    *public y static; son atributos del método. 
+    *String[] args; son los argumentos que recibe el método main, 
+     en tipo arreglo. 
+    */
+    public static void main(String[] args) {
+        
+        /*
+        *System; Al empezar con mayusculas nos indica que es una clase, 
+         de la clase system.
+        *out; Submódulo que manda una salida.
+        *println; es la forma de mostrar en pantalla.
+        *En conjunto, se ordena que de la clase system salga un print, 
+         que es la forma de imprimir en pantalla lo que se encuentre 
+         dentro del paréntesis entre comillas.
+        */
+        System.out.println("Hola mundo!!");
+    }
+    
+}
