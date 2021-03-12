474a2e678fbd031e6ff0310ec93a6bb8f0093d3a
Edgar Rivera <80140422+edgar06un@users.noreply.github.com>
2021-03-12 15:04:15 -0600

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mppo_2;
+
+/**
+ *
+ * @author edgarriv69
+ */
+public class NewClass {
+    public static void main(String[] args){
+        //sout tab
+        System.out.println("");
+        //Investigar los atajos de teclado snippets
+        //for tab
+        for (int i = 0; i < 10; i++) {
+  
+            break;
+        }
+    }
+    
+}
